adb80c74961a759b5ce9bf8e1e1324f1045dd7cb
[liam-coll] <lrjcoll@gmail.com>
2023-06-06 22:32:52 +1200

    more mobile fixes

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -11,14 +11,20 @@ export const Contact: React.FC<any> = (props: any) => {
   return (
     <Container>
       <h2>Contact</h2>
-      <p className="icons d-flex gap-0 gap-lg-4 justify-content-center flex-column flex-lg-row align-items-center">
-        <a href="mailto:liam@liamcoll.com" title="Email" target="_blank">
+      <Container className="icons d-flex gap-2 gap-lg-4 justify-content-center flex-column flex-lg-row align-items-center align-items-lg-stretch">
+        <a
+          href="mailto:liam@liamcoll.com"
+          title="Email"
+          target="_blank"
+          className="lh-1"
+        >
           <FontAwesomeIcon icon={faEnvelope} />
         </a>
         <a
           href="https://www.linkedin.com/in/liam-coll/"
           title="Linkedin"
           target="_blank"
+          className="lh-1"
         >
           <FontAwesomeIcon icon={faLinkedin} />
         </a>
@@ -26,10 +32,11 @@ export const Contact: React.FC<any> = (props: any) => {
           href="https://www.youtube.com/channel/UCbChr404bLEXYuR_uXWnb6A"
           title="YouTube"
           target="_blank"
+          className="lh-1"
         >
           <FontAwesomeIcon icon={faYoutube} enableBackground={1} />
         </a>
-      </p>
+      </Container>
     </Container>
   );
 };
